2391590060eac9051c59fb9c61af9b8c3fb9c96f
Alex Cook <apcook7104@gmail.com>
2021-03-12 23:16:13 -0800

    Add default nacl

--- aws/ec2/vpc.tf
+++ aws/ec2/vpc.tf
@@ -3,8 +3,7 @@ data "aws_availability_zones" "available" {
 }
 
 resource "aws_vpc" "vpc" {
-  cidr_block                       = "10.74.0.0/16"
-  assign_generated_ipv6_cidr_block = true
+  cidr_block = "10.74.0.0/16"
 
   tags = {
     Name = format("%s-vpc", var.name_prefix)
@@ -21,6 +20,7 @@ resource "aws_internet_gateway" "igw" {
 
 resource "aws_default_route_table" "rtb" {
   default_route_table_id = aws_vpc.vpc.default_route_table_id
+
   route = [{
     cidr_block                = "0.0.0.0/0"
     egress_only_gateway_id    = null
@@ -32,20 +32,36 @@ resource "aws_default_route_table" "rtb" {
     transit_gateway_id        = null
     vpc_endpoint_id           = null
     vpc_peering_connection_id = null
-    }, {
-    cidr_block                = null
-    egress_only_gateway_id    = null
-    gateway_id                = aws_internet_gateway.igw.id
-    instance_id               = null
-    ipv6_cidr_block           = "::/0"
-    nat_gateway_id            = null
-    network_interface_id      = null
-    transit_gateway_id        = null
-    vpc_endpoint_id           = null
-    vpc_peering_connection_id = null
   }]
 
   tags = {
     Name = format("%s-rtb", var.name_prefix)
+  }
+}
+
+resource "aws_default_network_acl" "nacl" {
+  default_network_acl_id = aws_vpc.vpc.default_network_acl_id
+  // subnet_ids             = ["value"]
+
+  ingress {
+    protocol   = -1
+    rule_no    = 100
+    action     = "allow"
+    cidr_block = "0.0.0.0/0"
+    from_port  = 0
+    to_port    = 0
+  }
+
+  egress {
+    protocol   = -1
+    rule_no    = 100
+    action     = "allow"
+    cidr_block = "0.0.0.0/0"
+    from_port  = 0
+    to_port    = 0
+  }
+
+  tags = {
+    Name = format("%s-nacl", var.name_prefix)
   }
 }
